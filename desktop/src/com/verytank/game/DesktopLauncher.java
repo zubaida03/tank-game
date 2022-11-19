d39bd4e806022c1ccde06c7751f9704832d4f2ec
vartika2401 <vartika3004@gmail.com>
2022-11-19 09:47:27 +0530

    test

---
+++ desktop/src/com/verytank/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.verytank.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.verytank.game.Very_Tank;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("My GDX Game");
+		new Lwjgl3Application(new Very_Tank(), config);
+	}
+}
---
+++ ios/src/com/verytank/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.verytank.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.verytank.game.Very_Tank;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Very_Tank(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
