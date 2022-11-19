d5e2d31d25cb72855bd4e3fa2d4bc8ae11bff5bf
vartika2401 <vartika3004@gmail.com>
2022-11-19 12:04:03 +0530

    LoadScreen done

---
+++ core/src/com/verytank/game/LoadScreen.java
@@ -0,0 +1,65 @@
+package com.verytank.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import java.awt.*;
+
+public class LoadScreen extends ScreenAdapter {
+    Very_Tank game;
+    private SpriteBatch batch;
+    private Texture load_sign;
+    private Rectangle load_sign_rect;
+    private Texture load_img;
+    private Rectangle load_img_rect;
+    private Pixmap pixmap;
+
+    public LoadScreen(Very_Tank game) {
+        this.game = game;
+        batch = new SpriteBatch();
+//        pixmap = new Pixmap(100,20, Pixmap.Format.RGBA8888);
+//        pixmap.setColor(Color.WHITE);
+//        pixmap.fill();
+//        TextureRegionDrawable drawable = new TextureRegionDrawable(new Texture(pixmap));
+//        ProgressBar.ProgressBarStyle style = new ProgressBar.ProgressBarStyle(drawable, drawable);
+//        style.background = drawable;
+        load_img = new Texture(Gdx.files.internal("loader.png"));
+        load_img_rect = new Rectangle(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        load_sign = new Texture(Gdx.files.internal("load_line.png"));
+        load_sign_rect = new Rectangle(710, 0, 500, 300);
+    }
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+//        batch.begin();
+        game.batch.draw(load_img, load_img_rect.x, load_img_rect.y, load_img_rect.width, load_img_rect.height);
+        game.batch.draw(load_sign, load_sign_rect.x, load_sign_rect.y, load_sign_rect.width, load_sign_rect.height);
+        String text = "Loading...";
+        game.font.setColor(Color.BLACK);
+        game.font.getScaleY();
+//        game.font.draw(game.batch, text, 900, 150);
+//        game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
+//        game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+//        batch.end();
+        game.batch.end();
+
+
+    }
+}
+
