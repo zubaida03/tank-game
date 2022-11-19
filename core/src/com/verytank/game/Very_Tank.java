d287747bf623fa978710de7db3eedccd1b6862dd
vartika2401 <vartika3004@gmail.com>
2022-11-19 12:00:09 +0530

    LoadScreen done LoginScreen in progress

--- core/src/com/verytank/game/Very_Tank.java
+++ core/src/com/verytank/game/Very_Tank.java
@@ -1,27 +1,42 @@
 package com.verytank.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Very_Tank extends ApplicationAdapter {
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class Very_Tank extends Game {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
+	ShapeRenderer shapeRenderer;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		ShapeRenderer shapeRenderer = new ShapeRenderer();
+		font = new BitmapFont();
+		setScreen(new LoginScreen(this));
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
+//	@Override
+//	public void render () {
+//		batch.dispose();
+////		shapeRenderer.dispose();
+//		font.dispose();
+//	}
 	
 	@Override
 	public void dispose () {
